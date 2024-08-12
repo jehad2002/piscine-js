5a88d17bc37e155b0f75d2751c947f5f1ecae56a
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-12 15:17:14 +0300

    jehad

---
+++ declarations.js
@@ -0,0 +1,22 @@
+const escapeStr = "`, \, /, '";
+const arr = [4, "2"];
+const obj = {
+    str: "string",
+    num: 1,
+    bool: true,
+    undef:undefined,
+};
+const nested = {
+    arr: [4, undefined, "2"]
+    obj: {
+        str: "string",
+        num: 1,
+        bool: true,
+    },
+};
+Object.freeze(nested);
+Object.freeze(nested.obj);
+Object.freeze(nested.arr);
+Object.freeze(obj);
+Object.freeze(arr);
+
