7ed9d3fad3a3301dd6c8b9ea7d969589663f7020
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-12 14:37:17 +0300

    jehad

---
+++  primitives.js
@@ -0,0 +1,15 @@
+// document.write("Hello"); string write
+// document.write(typeof 10) type int
+// document.write( 10 + 50) output 60
+// document.write('10' + '50') output 1050
+// document.write(typeof true) Boolean
+// let x = 10; var
+// document.write(typeof x) undefined
+/* var name;
+name = 'jehad';
+document.write(name)*/
+// const v = 30; 
+const s = 'hello';
+const i = 10;
+const und = undefined;
+const b = true;
