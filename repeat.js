d31ea08abedde0300eb563d1c848f417c9aa7224
jehad <your.email@example.com>
2024-08-14 17:55:52 +0300

    jehad

---
+++ repeat.js
@@ -0,0 +1,70 @@
+function repeat(str, number) {
+var r = "";
+for (var i = 0; i < number; i++) {
+    r += str;
+}
+return r;
+}
+// console.log(repeat("Hello", 3)); HelloHelloHello
+
+
+// print from 0 to 9
+// for (let i = 0; i <= 9; i+=)
+// {
+// console.log(i)
+// }
+//======================================
+// print
+// j 
+// e
+// h
+// a
+// d
+// let x ='jehad'
+// for (let i = 0; i < x.length; i++)
+// {
+// console.log(x[i])
+// }
+//=========================================
+
+// let cars = ['BMW','MERC','KIA']
+// let models = [2020,2022,2024]
+// let colors = ['black','white','blue']
+// for (let i = 0; i < cars.length; i++)
+// {
+//     console.log(`cars : ${cars[i]}`)
+//     for (let j = 0; j < models.length; j++)
+// {
+//     console.log(`models : ${models[j]}`)
+// }
+// for (let x = 0; x < colors.length; x++)
+// {
+//     console.log(`colors : ${colors[x]}`)
+// }
+// console.log('________________________')
+// }
+// output 
+// cars : BMW
+// models : 2020
+// models : 2022
+// models : 2024
+// colors : black
+// colors : white
+// colors : blue
+// ________________________
+// cars : MERC
+// models : 2020
+// models : 2022
+// models : 2024
+// colors : black
+// colors : white
+// colors : blue
+// ________________________
+// cars : KIA
+// models : 2020
+// models : 2022
+// models : 2024
+// colors : black
+// colors : white
+// colors : blue
+// ________________________
