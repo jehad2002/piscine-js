522fa196888f3940bbe7ca596cb74b4fbb3e4e1d
jehad <your.email@example.com>
2024-08-14 19:49:47 +0300

    jehad

--- keep-cut.js
+++ keep-cut.js
@@ -1,27 +1,27 @@
 function cutFirst(str) {
-    return str.slice(2)
+    return str.slice(2);
 }
 // console.log(cutFirst("Hello, World!"));  Output: "llo, World!"
 // console.log(cutFirst("JavaScript"));     Output: "vaScript"
 
 function cutLast(str) {
-    return str.slice(0, -2)
+    return str.slice(0, -2);
 }
 // console.log(cutLast("Hello, World!"));  Output: "Hello, World"
 // console.log(cutLast("JavaScript"));     Output: "JavaScrip"
 
 function cutFirstLast(str) {
-    return str.slice(2, -2)
+    return str.slice(2, -2);
 }
 // console.log(cutFirstLast("Hello, World!"));  Output: "llo, World"
 // console.log(cutFirstLast("JavaScript"));     Output: "vaScrip"
 function keepFirst(str) {
-    return str.slice(0,2)
+    return str.slice(0,2);
 }
 // console.log(keepFirst('abcdef'));  // Output: 'ab'
 
 function keepLast(str) {
-    return str.slice(-2)
+    return str.slice(-2);
 }
 // console.log(keepLast("Hello, World!"));  Output: "! "
 // console.log(keepLast("JavaScript"));     Output: "pt"
@@ -30,7 +30,7 @@ function keepLast(str) {
 
 function keepFirstLast(str) {
     if (str.length <= 3) {
-        // If the string is 2 characters or shorter, return the string as is
+        // If the string is 3 characters or shorter, return the string as is
         return str;
     }
     return str.slice(0, 2) + str.slice(-2);
