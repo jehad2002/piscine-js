0d9e8b49405ca42bd475a549950501842fe69900
jehad <your.email@example.com>
2024-08-14 18:51:47 +0300

    jehad

--- slicer.js
+++ slicer.js
@@ -22,17 +22,17 @@ function slice(input, start, end) {
         }
     }
     return res;
-}
-// Example 1: Slicing a string
-let str = "Hello, World!";
-console.log(slice(str, 0, 5)); // Output: "Hello"
-console.log(slice(str, -6));   // Output: "World!"
+ }
+// // Example 1: Slicing a string
+// let str = "Hello, World!";
+// console.log(slice(str, 0, 5)); // Output: "Hello"
+// console.log(slice(str, -6));   // Output: "World!"
 
-// Example 2: Slicing an array
-let arr = [1, 2, 3, 4, 5];
-console.log(slice(arr, 1, 3));  // Output: [2, 3]
-console.log(slice(arr, -3));    // Output: [3, 4, 5]
+// // Example 2: Slicing an array
+// let arr = [1, 2, 3, 4, 5];
+// console.log(slice(arr, 1, 3));  // Output: [2, 3]
+// console.log(slice(arr, -3));    // Output: [3, 4, 5]
 
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 
-console.log(animals.slice(2)); //Output [ 'camel', 'duck', 'elephant' ]
+// console.log(animals.slice(2)); //Output [ 'camel', 'duck', 'elephant' ]
