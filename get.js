9f790b48bd9d14e227f457c2249ac5eabefc1063
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-15 15:53:26 +0300

    jehad

--- get.js
+++ get.js
@@ -1,4 +1,7 @@
 function get(path,src) {
+    if (typeof path !== 'string') {
+        throw new TypeError('Path must be a string');
+    }
     return path.split("-").reduce(function (obj, key) {
     if (obj === undefined) {
         return undefined;
