68c0192905448e80a18710f6360e91e9da827477
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-15 14:19:29 +0300

    jehad

--- chunky.js
+++ chunky.js
@@ -1,10 +1,16 @@
 function chunk(arr, size) {
+    if (size <= 0) {
+        return [];
+    }
+
     var res = [];
-    for (var i = 0; i < arr.length; i+= size); {
+    for (var i = 0; i < arr.length; i += size) {
         res.push(arr.slice(i, i + size));
     }
     return res;
 }
-// var arrayToChunk = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// var chunkedArray = chunk(arrayToChunk, 3);
-// console.log(chunkedArray);
+// console.log(chunk([1, 2, 3, 4, 5], 2));  // Output: [[1, 2], [3, 4], [5]]
+// console.log(chunk([1, 2, 3, 4, 5], 3));  // Output: [[1, 2, 3], [4, 5]]
+// console.log(chunk([1, 2, 3, 4, 5], 1));  // Output: [[1], [2], [3], [4], [5]]
+// console.log(chunk([1, 2, 3, 4, 5], 0));  // Output: []
+// console.log(chunk([1, 2, 3, 4, 5], -1)); // Output: []
