3b5b34b2ef8777b95f04630737727f3e038cc146
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-15 16:02:03 +0300

    jehad

--- get.js
+++ get.js
@@ -1,16 +1,25 @@
 function get(src, path) {
-    if (typeof path !== 'string') {
-        throw new TypeError('Path must be a string');
-    }
-    return path.split("-").reduce(function (obj, key) {
-        if (obj === undefined) {
-            return undefined;
-        }
-        return obj[key];
-    }, src);
+    return path.split('.').reduce((acc, key) => acc && acc[key], src);
 }
+//===================================================================
+// Example usage:
+// const src = { nested: { key: 'peekaboo' } };
+// const path = 'nested.key';
+// console.log(get(src, path)); // -> 'peekaboo'
+//==============================================
+// function get(src, path) {
+//     if (typeof path !== 'string') {
+//         throw new TypeError('Path must be a string');
+//     }
+//     return path.split("-").reduce(function (obj, key) {
+//         if (obj === undefined) {
+//             return undefined;
+//         }
+//         return obj[key];
+//     }, src);
+// }
 
-// Example test
+ // Example test
 // try {
 //     const result = get({ key: 'value' }, 'key');
 //     console.log(result === 'value'); // Should log: true
