ab92d4d5bbf3c2ed5f71f408c3707b345eaa94c5
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-15 16:24:20 +0300

    jehad

--- pyramid.js
+++ pyramid.js
@@ -2,7 +2,7 @@ function pyramid(char,num) {
     let str = "";
 let space = " ".repeat(char.length);
 for(var i = 1; i<=num; i++) {
-str = str + space.repeat(num - 1) + char.repeat(2 * i - 1) + "\n"
+str = str + space.repeat(num - 1) + char.repeat(2 * i - 1) + "\n";
 }
 return str.slice(0,-1)
 
