36a5c246799f9568ebbbf4da910ac6599b5780e4
jehad <your.email@example.com>
2024-08-15 21:30:25 +0300

    jehad

--- pyramid.js
+++ pyramid.js
@@ -1,14 +1,14 @@
 function pyramid(char, num) {
     let str = "";
     for (let i = 1; i <= num; i++) {
-        let space = " ".repeat(num - i);       // Calculate leading spaces
-        let chars = char.repeat(2 * i - 1);    // Calculate the number of characters
-        str += space + chars + "\n";           // Construct the line and add a newline
+        let space = " ".repeat(num - i);       
+        let chars = char.repeat(2 * i - 1); 
+        str += space + chars + "\n";           
     }
-    return str;
+    return str.slice(0, -1);                   
 }
 
-// console.log(pyramid('*', 5));'
+// console.log(pyramid('*', 5));
 // Output: 
 //     *
 //    ***
