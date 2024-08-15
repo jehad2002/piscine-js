5957e60fc008880976b1a13991a6e414c7758340
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-15 15:59:06 +0300

    jehad

--- get.js
+++ get.js
@@ -1,11 +1,19 @@
-function get(path,src) {
+function get(src, path) {
     if (typeof path !== 'string') {
         throw new TypeError('Path must be a string');
     }
     return path.split("-").reduce(function (obj, key) {
-    if (obj === undefined) {
-        return undefined;
-    }
-    return obj[key];
-},src);
+        if (obj === undefined) {
+            return undefined;
+        }
+        return obj[key];
+    }, src);
 }
+
+// Example test
+// try {
+//     const result = get({ key: 'value' }, 'key');
+//     console.log(result === 'value'); // Should log: true
+// } catch (error) {
+//     console.error(error.message);
+// }
