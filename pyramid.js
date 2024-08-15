08c8caf0698bb78c4d8e65a88d2ba81591b77e52
jehad <your.email@example.com>
2024-08-15 21:33:21 +0300

    jehad

--- pyramid.js
+++ pyramid.js
@@ -2,10 +2,10 @@ function pyramid(char, num) {
     let str = "";
     for (let i = 1; i <= num; i++) {
         let space = " ".repeat(num - i);       
-        let chars = char.repeat(2 * i - 1); 
+        let chars = char.repeat(2 * i - 1);    
         str += space + chars + "\n";           
     }
-    return str.slice(0, -1);                   
+    return str.trim();                      
 }
 
 // console.log(pyramid('*', 5));
