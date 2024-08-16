06fe4dcc86b64354d483a16f127a7899a9ff8149
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-16 12:45:38 +0300

    jehad

--- find-expression.js
+++ find-expression.js
@@ -1,3 +1,29 @@
+// function findExpression(num) {
+//     for (let i = 0; i < 1000000; i++) {
+//         let copy = 1;
+//         let s = "1";
+//         while (copy !== num) {
+//             if (copy < num) {
+//                 if (Math.random() < 0.5) {
+//                     copy += 4;
+//                     s += " +4";
+//                 } else {
+//                     copy *= 2;
+//                     s += " *2";
+//                 }
+//             } else {
+//                 break;
+//             }
+//         }
+//         if (copy === num) {
+//             return s;
+//         }
+//     }
+//     return undefined;
+// }
+
+// console.log(findExpression(8)); // Output: "1 *2 *2 +4"
+
 function findExpression(num) {
     for (let i = 0; i < 1000000; i++) {
         let copy = 1;
@@ -6,10 +32,10 @@ function findExpression(num) {
             if (copy < num) {
                 if (Math.random() < 0.5) {
                     copy += 4;
-                    s += " +4";
+                    s += " add4";
                 } else {
                     copy *= 2;
-                    s += " *2";
+                    s += " mul2";
                 }
             } else {
                 break;
@@ -22,4 +48,4 @@ function findExpression(num) {
     return undefined;
 }
 
-console.log(findExpression(8)); // Output: "1 *2 *2 +4"
+console.log(findExpression(8)); // Output: "1 mul2 mul2 add4"
