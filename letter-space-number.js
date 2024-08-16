30f3cfa7288b3fb82005198af1d0d038fe0d22c5
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-16 15:09:40 +0300

    jehad

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,4 +1,4 @@
-function letterSpaceNumber(str) {
-let str = str.match(/[a-z] [0-9][?][a-z0-9]/gi);
+function letterSpaceNumber(inputStr) {
+let inputStr = inputStr.match(/[a-z] [0-9][?][a-z0-9]/gi);
 return arr !== null ?arr : [];
 }
