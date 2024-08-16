7d2b3e91a098564dbbfcd0e0d801c8babacdd5aa
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-16 12:43:08 +0300

    jehad

--- find-expression.js
+++ find-expression.js
@@ -2,19 +2,24 @@ function findExpression(num) {
     for (let i = 0; i < 1000000; i++) {
         let copy = 1;
         let s = "1";
-        while (copy <= num) {
-            if (copy === num) {
-                return s;
-            }
-            if (Math.random < 0.5) {
-                copy += 4
-                s += " +4"
+        while (copy !== num) {
+            if (copy < num) {
+                if (Math.random() < 0.5) {
+                    copy += 4;
+                    s += " +4";
+                } else {
+                    copy *= 2;
+                    s += " *2";
+                }
             } else {
-                copy *= 2
-                s += " +2"
+                break;
             }
+        }
+        if (copy === num) {
+            return s;
         }
     }
     return undefined;
 }
-//1 *2 *2 +4.
+
+console.log(findExpression(8)); // Output: "1 *2 *2 +4"
