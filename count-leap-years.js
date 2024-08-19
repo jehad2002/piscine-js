d64caf3d4b1287b060c89cdcb9a31c75b156519e
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-19 14:22:12 +0300

    jehad

--- count-leap-years.js
+++ count-leap-years.js
@@ -2,8 +2,8 @@ function countLeapYears(data) {
     let years = 0;
     for(let year = 1; year < Date.getFullYear(); year++) {
         if (year % 4 === 0 && (year % 100 !== 0  || year % 400 === 0 )) {
-            years++
+            years++;
         }
     }
-    return years
+    return years;
 }
