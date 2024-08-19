74ec7489d73074a5f55302dce5e098dec437d2fd
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-19 12:25:48 +0300

    jehad

--- dr-strange.js
+++ dr-strange.js
@@ -1,23 +1,26 @@
-function addWeek(data) {
+function addWeek(date) {
     const weeks = {
-        0 : "monday",
-        1 : "tuesday",
-        2 : "wednesday",
-        3 : "thurssday",
-        4 : "friday",
-        5 : "saturday",
-        6 : "sunday",
-        7 : "secMonday",
-        8 : "secTuesday",
-        9 : "secWednesday",
-        10 : "secThursday",
-        11 : "secFriday",
-        12 : "secSaturday",
-        13 : "secSunday",
+        0: "monday",
+        1: "tuesday",
+        2: "wednesday",
+        3: "thurssday",
+        4: "friday",
+        5: "saturday",
+        6: "sunday",
+        7: "secMonday",
+        8: "secTuesday",
+        9: "secWednesday",
+        10: "secThursday",
+        11: "secFriday",
+        12: "secSaturday",
+        13: "secSunday",
     };
-    let time = Date.getTime() + 621355968000000; // melSec  from 1/1/1970  
-    return weeks (time / 864000000 % 14) // melSec in 1 day 
+
+    let time = date.getTime() + 62135596800000; //add milliseconds
+    return weeks[Math.floor((time / 86400000) % 14)]; //calculation for days
 }
+
 function timeTravel(data) {
-    return new data(date.date.setHours(date.hours, date.minute, date.secound));
+    data.date.setHours(data.hours, data.minute, data.secound);
+    return new Date(data.date);
 }
