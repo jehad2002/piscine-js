542630d10f064ea92391a8435de7e48aac5553f8
jehad <your.email@example.com>
2024-08-20 16:23:52 +0300

    jehad

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,18 +1,24 @@
 function sunnySunday(date) {
-    let counter = 0;
     const week = [
         "Monday",
         "Tuesday",
         "Wednesday",
         "Thursday",
         "Friday",
-        "Saturday",
-        "Sunday"
+        "Saturday"
     ];
+
     let start = new Date("0001-01-01T00:00:00");
+    let counter = 0;
+
     while (start < date) {
         start.setDate(start.getDate() + 1);
         counter++;
+        if (counter % 7 === 6) {
+            // Skip Sundays by not incrementing the counter on Sundays
+            start.setDate(start.getDate() + 1);
+        }
     }
-    return week[counter % 7];
+
+    return week[counter % 6];
 }
