e14c52f4dddfd5e98439c66e5f3f2f9d9f2095d6
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-21 12:10:01 +0300

    jehad

--- call-me-maybe/for-each.js
+++ call-me-maybe/for-each.js
@@ -3,31 +3,6 @@ function forEach(array, action) {
         action(array[i], i, array);
     }
 }
-//==================================Filtter==========================
-
-// function filter(arr, fn) {
-//     var result = [];
-//     for (var i = 0; i < arr.length; i++) {
-//         if (fn(arr[i], i, arr)) {
-//             result.push(arr[i]);
-//         }
-//     }
-//     return result;
-// }
-
-// function reject(arr, fn) {
-//     var result = [];
-//     for (var i = 0; i < arr.length; i++) {
-//         if (!fn(arr[i], i, arr)) {
-//             result.push(arr[i]);
-//         }
-//     }
-//     return result;
-// }
-
-// function partition(arr, fn) {
-//     return [filter(arr, fn), reject(arr, fn)];
-// }
 //======================reduce=======================================
 
 // function fold(arr, f, acc) {
