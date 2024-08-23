666801728b7d75048309a347ee588efe71c2ac4e
jehad <your.email@example.com>
2024-08-23 15:30:38 +0300

    jehad

--- call-me-maybe/for-each.js
+++ call-me-maybe/for-each.js
@@ -5,14 +5,7 @@ function forEach(array, action) {
 }
 
 
-// function hasCity(country, cities) {
-//     return function (city) {
-//         if (cities.indexOf(city) === -1) {
-//             return city + " is not a city from " + country;
-//         }
-//         return city + " is a city from " + country;
-//     };
-// }
+
 //===========================flow================================
 
 // function flow(arr) {
