08808d2978aaa763d68df398547daa35babd6540
jehad <your.email@example.com>
2024-08-23 15:27:56 +0300

    jehad

--- call-me-maybe/for-each.js
+++ call-me-maybe/for-each.js
@@ -3,25 +3,6 @@ function forEach(array, action) {
         action(array[i], i, array);
     }
 }
-//===============quanfiltters===========================
-
-// function every(array, test) {
-//     for (var i = 0; i < array.length; i++) {
-//         if (!test(array[i])) return false;
-//     }
-//     return true;
-// }
-
-// function some(array, test) {
-//     for (var i = 0; i < array.length; i++) {
-//         if (test(array[i])) return true;
-//     }
-//     return false;
-// }
-
-// function none(array, test) {
-//     return !some(array, test);
-// }
 //=====================sweet========================
 
 // function mult2(x) {
