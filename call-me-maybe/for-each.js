79c22c6efb2ed34c5bb7f2dedcf30d648cb7dee1
jehad <your.email@example.com>
2024-08-23 15:32:08 +0300

    jehad

--- call-me-maybe/for-each.js
+++ call-me-maybe/for-each.js
@@ -8,14 +8,7 @@ function forEach(array, action) {
 
 //===========================flow================================
 
-// function flow(arr) {
-//     return function (...args) {
-//         if (args.length > 1) {
-//             args = [arr[0](...args)];
-//         }
-//         return arr.reduce((acc, fn) => fn(acc), args[0]);
-//     };
-// }
+
 //=============================curr=========================
 
 // function currify(fn) {
