2a561770616e2775d259dfcc368115f80a709669
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-26 15:53:49 +0300

    jehad

--- pick-omit.js
+++ pick-omit.js
@@ -1,20 +1,35 @@
+// Function to pick specified keys from an object
 function pick(obj, keys) {
-    const obj2 = {}
-    let keysObj = Object.keys(obj)
-    keysObj.forEach(key => {
-        if (key == keys) {
-            obj2[key] = obj[key]
+    const obj2 = {};
+    Object.keys(obj).forEach(key => {
+        if (keys.includes(key)) {
+            obj2[key] = obj[key];
         }
-    })
-    return obj2
+    });
+    return obj2;
 }
+
+// Function to omit specified keys from an object
 function omit(obj, keys) {
-    const obj2 = {}
-    let keysObj = Object.keys(obj)
-    keysObj.forEach(key => {
-        if (key != keys) {
-            obj2[key] = obj[key]
+    const obj2 = {};
+    Object.keys(obj).forEach(key => {
+        if (!keys.includes(key)) {
+            obj2[key] = obj[key];
         }
-    })
-    return obj2
-} 
+    });
+    return obj2;
+}
+
+// // Example usage:
+// const originalObject = {
+//     a: 1,
+//     b: 2,
+//     c: 3,
+//     d: 4
+// };
+
+// const pickedObject = pick(originalObject, ['a', 'c']);
+// const omittedObject = omit(originalObject, ['b', 'd']);
+
+// console.log('Picked Object:', pickedObject);  // { a: 1, c: 3 }
+// console.log('Omitted Object:', omittedObject); // { a: 1, c: 3 }
