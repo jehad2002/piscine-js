889dc10c7bdb1418750ec1efa2dd17a4972d59d9
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-27 10:52:32 +0300

    jehad

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,28 +1,28 @@
-function filter(obj, pre) {
-    let f = {};
-    for (let key in obj) {
-        if (pre(key)) {
-            f[key] = obj[key];
-        }
-    }
-    return f;
-}
+// function filter(obj, pre) {
+//     let f = {};
+//     for (let key in obj) {
+//         if (pre(key)) {
+//             f[key] = obj[key];
+//         }
+//     }
+//     return f;
+// }
 
-function map(obj, CB) {
-    let map = {};
-    for (let key in obj) {
-        map[CB(key)] = obj[key];
-    }
-    return map;
-}
+// function map(obj, CB) {
+//     let map = {};
+//     for (let key in obj) {
+//         map[CB(key)] = obj[key];
+//     }
+//     return map;
+// }
 
-function reduce(obj, CB, initialValue = '') {
-    let acc = initialValue;
-    for (let key in obj) {
-        acc = CB(acc, key);
-    }
-    return acc;
-}
+// function reduce(obj, CB, initialValue = '') {
+//     let acc = initialValue;
+//     for (let key in obj) {
+//         acc = CB(acc, key);
+//     }
+//     return acc;
+// }
 
 // Example usage:
 // const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
@@ -36,3 +36,29 @@ function reduce(obj, CB, initialValue = '') {
 
 // console.log(reduce(nutrients, (acc, key) => acc.concat(', ', key), '').slice(2));
 // Output: carbohydrates, protein, fat
+
+function filterKeys(obj, pre) {
+    let f = {};
+    for (let key in obj) {
+        if (pre(key)) {
+            f[key] = obj[key];
+        }
+    }
+    return f;
+}
+
+function mapKeys(obj, CB) {
+    let map = {};
+    for (let key in obj) {
+        map[CB(key)] = obj[key];
+    }
+    return map;
+}
+
+function reduceKeys(obj, CB, initialValue = '') {
+    let acc = initialValue;
+    for (let key in obj) {
+        acc = CB(acc, key);
+    }
+    return acc;
+}
