db38c187de9a310484ba10e2690c860b75654957
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-27 10:06:48 +0300

    jehad

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,28 +1,38 @@
-function filterKeys(obj, predicate) {
+function filterKeys(obj, pre) {
     let f = {};
     for (let key in obj) {
-        if (predicate(key)) {
+        if (pre(key)) {
             f[key] = obj[key];
         }
     }
     return f;
 }
 
+// // Example usage
+// const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
+// console.log(filterKeys(nutrients, (key) => /protein/.test(key)));
+// // Output: { protein: 20 }
 
-function map(obj, CB) {
-    let res = {};
+function mapKeys(obj, CB) {
+    let map = {};
     for (let key in obj) {
-        res[key] = CB(obj[key]);
+        map[CB(key)] = obj[key];
     }
-    return res;
+    return map;
 }
 
-function reduce(obj, CB, acc) {
-    if (acc === undefined) {
-        acc = 0;
-    }
+// // Example usage
+// console.log(mapKeys(nutrients, (key) => `-${key}`));
+// // Output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
+
+function reduceKeys(obj, CB, initialValue) {
+    let acc = initialValue;
     for (let key in obj) {
-        acc = CB(acc, obj[key]);
+        acc = CB(acc, key);
     }
     return acc;
 }
+
+// // Example usage
+// console.log(reduceKeys(nutrients, (acc, key) => acc.concat(', ', key), '').slice(2));
+// // Output: carbohydrates, protein, fat
