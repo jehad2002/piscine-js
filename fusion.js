256fe5ad3835d4336104bf449b98aa076663e1ed
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-27 09:46:48 +0300

    jehad

--- fusion.js
+++ fusion.js
@@ -1,30 +1,30 @@
 function fusion(obj1, obj2) {
-    var fusioned = {};
+    var f = {};
     for (var key in obj1) {
         if (!obj1.hasOwnProperty(key)) continue;
         if (obj2.hasOwnProperty(key)) {
             if (is.obj(obj1[key]) && is.obj(obj2[key])) {
-                fusioned[key] = fusion(obj1[key], obj2[key]);
+                f[key] = fusion(obj1[key], obj2[key]);
             } else if (is.arr(obj1[key]) && is.arr(obj2[key])) {
-                fusioned[key] = obj1[key].concat(obj2[key]);
+                f[key] = obj1[key].concat(obj2[key]);
             } else if (is.num(obj1[key]) && is.num(obj2[key])) {
-                fusioned[key] = obj1[key] + obj2[key];
+                f[key] = obj1[key] + obj2[key];
             } else if (is.str(obj1[key]) && is.str(obj2[key])) {
-                fusioned[key] = obj1[key] + " " + obj2[key];
+                f[key] = obj1[key] + " " + obj2[key];
             } else {
-                fusioned[key] = obj2[key];
+                f[key] = obj2[key];
             }
         } else {
-            fusioned[key] = obj1[key];
+            f[key] = obj1[key];
         }
     }
     for (var key in obj2) {
         if (!obj2.hasOwnProperty(key)) continue;
         if (!obj1.hasOwnProperty(key)) {
-            fusioned[key] = obj2[key];
+            f[key] = obj2[key];
         }
     }
-    return fusioned;
+    return f;
 }
 
 const is = {};
