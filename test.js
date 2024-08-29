0ccb148913d85d09917a846658b6711587ab5cf1
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-29 13:57:12 +0300

    jehad

--- test.js
+++ test.js
@@ -1,23 +1,4 @@
-// function interpolation({
-//     step = 0,
-//     start = 0,
-//     end = 0,
-//     callback = () => {},
-//     duration = 0,
-// } = {}) {
-//     const delta = (end - start) / step;
-//     let current = start;
-//     let i = 0;
-//     const timer = setInterval(() => {
-//         if (i < step) {
-//             callback([current, (duration / step) * (i + 1)]);
-//             current += delta;
-//             i++;
-//         } else {
-//             clearInterval(timer);
-//         }
-//     }, duration / step);
-// }
+
 //=============================================================
 
 // async function getJSON(path = '', params = {}) {
