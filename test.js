8c65f58f8b019d204366aabdb47115c0c8e425d5
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-08-29 14:32:28 +0300

    jehad

--- test.js
+++ test.js
@@ -1,33 +1,3 @@
-
-//=============================================================
-
-// async function getJSON(path = '', params = {}) {
-//     const url =
-//         path +
-//         '?' +
-//         Object.keys(params)
-//             .map((key) => {
-//                 return (
-//                     key.replace(' ', '+') +
-//                     '=' +
-//                     params[key].toString().replace(' ', '+')
-//                 );
-//             })
-//             .join('&');
-//     const res = await fetch(url).then((response) => {
-//         if (response.ok) {
-//             return response.json();
-//         } else {
-//             throw new Error(response.statusText);
-//         }
-//     });
-//     if (res.error) {
-//         throw new Error(res.error);
-//     }
-//     return res.data;
-// }
-//==============================================
-
 // async function isWinner(country) {
 //     try {
 //         country = await db.getWinner(country);
