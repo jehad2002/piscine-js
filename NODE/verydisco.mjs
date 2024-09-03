55ffd4f1c218f8cb2396d2fc02c05be0607b8c61
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2024-09-03 11:20:01 +0300

    jehad

---
+++ NODE/verydisco.mjs
@@ -0,0 +1,30 @@
+const transformWord = (word) => {
+    const len = word.length;
+    const halfLen = Math.ceil(len / 2);
+    const firstHalf = word.slice(0, halfLen);
+    const secondHalf = word.slice(halfLen);
+    return secondHalf + firstHalf;
+};
+
+const main = () => {
+    const argument = process.argv[2];
+
+    if (!argument) {
+        console.log("Please provide an argument.");
+        return;
+    }
+
+    if (argument.includes(' ')) {
+        const words = argument.split(' ');
+        const transformedWords = words.map(transformWord);
+        const resultSentence = transformedWords.join(' ');
+        console.log(resultSentence);
+    } else {
+        const resultWord = transformWord(argument);
+        console.log(resultWord);
+    }
+};
+
+main();
+
+//node verydisco.mjs "Node is awesome"
