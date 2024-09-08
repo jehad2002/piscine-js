89337c884514bdc1adf27045b508416ff387399a
jehad <your.email@example.com>
2024-09-08 16:00:33 +0300

    jehad

--- uninvited.mjs
+++ uninvited.mjs
@@ -1,6 +1,11 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 5000;
 
